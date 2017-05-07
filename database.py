062c0290f5f94e0c5c926d3922633e0b89504857
Daniel Revie <Daniel Revie>
2017-05-07 18:08:21 -0400

    update

--- database.py
+++ database.py
@@ -36,8 +36,8 @@ class Hotel_DB:
             roomID VARCHAR,
             breakfastsOrdered VARCHAR,
             servicesOrdered VARCHAR,
-            paymentOwed INT
-            FOREIGN KEY(customerID) REFERENCES customer(cid)
+            paymentOwed INT,
+            FOREIGN KEY(customerID) REFERENCES customer(cid),
             FOREIGN KEY(roomID) REFERENCES rooms(roomID)
             )''')
 
@@ -53,88 +53,46 @@ class Hotel_DB:
         self.db.commit()
 
     def insertTestValues(self):
-        meetings = [
+        customers = [
             {
-                'id': 1,
-                'ownerID': 1
+                'firstName': "Daniel",
+                'lastName': "Revie",
+                'phone': "2016753642",
+                'email': "daniel.revie1@gmail.com",
+                'password': "Mr J"
+
             },
             {
-                'id': 2,
-                'ownerID': 2
+                'firstName': "Daniel",
+                'lastName': "Revie",
+                'phone': "2016753642",
+                'email': "daniel.revie1@gmail.com",
+                'password': "Mr J"
+
             },
             {
-                'id': 3,
-                'ownerID': 3
+                'firstName': "Daniel",
+                'lastName': "Revie",
+                'phone': "2016753642",
+                'email': "daniel.revie1@gmail.com",
+                'password': "Mr J"
+
             },
             {
-                'id': 4,
-                'ownerID': 4
-            }
+                'firstName': "Daniel",
+                'lastName': "Revie",
+                'phone': "2016753642",
+                'email': "daniel.revie1@gmail.com",
+                'password': "Mr J"
+
+            },
+
         ]
 
-        hosts = [
-            {
-                'id': 1,
-                'email': u'test1@gmail.com'
-            },
-            {
-                'id': 2,
-                'email': u'test2@gmail.com'
-            },
-            {
-                'id': 3,
-                'email': u'test3@gmail.com'
-            },
-            {
-                'id': 4,
-                'email': u'test4@gmail.com'
-            },
-        ]
-
-        recordings = [
-            {
-                'url': u'https://www.meeting1.com',
-                'protectionLevel': True,
-                'password': u'cats',
-                'meetingID': 1
-            },
-            {
-                'url': u'https://www.meeting2.com',
-                'protectionLevel': False,
-                'password': u'',
-                'meetingID': 2
-            },
-            {
-                'url': u'https://www.meeting3.com',
-                'protectionLevel': True,
-                'password': u'dogs',
-                'meetingID': 3
-            },
-            {
-                'url': u'https://www.meeting4.com',
-                'protectionLevel': False,
-                'password': u'',
-                'meetingID': 4
-            },
-        ]
-
-        # Populate meetings
-        self.cursor.execute(''' INSERT INTO meetings(id, ownerID) VALUES (?,?)''', (meetings[0]['id'], meetings[0]['ownerID']))
-        self.cursor.execute(''' INSERT INTO meetings(id, ownerID) VALUES (?,?)''', (meetings[1]['id'], meetings[1]['ownerID']))
-        self.cursor.execute(''' INSERT INTO meetings(id, ownerID) VALUES (?,?)''', (meetings[2]['id'], meetings[2]['ownerID']))
-        self.cursor.execute(''' INSERT INTO meetings(id, ownerID) VALUES (?,?)''', (meetings[3]['id'], meetings[3]['ownerID']))
-
-        # Populate hosts
-        self.cursor.execute(''' INSERT INTO hosts(id, email) VALUES (?,?) ''', (hosts[0]['id'], hosts[0]['email']))
-        self.cursor.execute(''' INSERT INTO hosts(id, email) VALUES (?,?) ''', (hosts[1]['id'], hosts[1]['email']))
-        self.cursor.execute(''' INSERT INTO hosts(id, email) VALUES (?,?) ''', (hosts[2]['id'], hosts[2]['email']))
-        self.cursor.execute(''' INSERT INTO hosts(id, email) VALUES (?,?) ''', (hosts[3]['id'], hosts[3]['email']))
-
-        # Populate recordings
-        self.cursor.execute(''' INSERT INTO recordings(url, protectionLevel, password, meetingID) VALUES (?,?,?,?) ''', (recordings[0]['url'], recordings[0]['protectionLevel'], recordings[0]['password'], recordings[0]['meetingID']))
-        self.cursor.execute(''' INSERT INTO recordings(url, protectionLevel, password, meetingID) VALUES (?,?,?,?) ''', (recordings[1]['url'], recordings[1]['protectionLevel'], recordings[1]['password'], recordings[1]['meetingID']))
-        self.cursor.execute(''' INSERT INTO recordings(url, protectionLevel, password, meetingID) VALUES (?,?,?,?) ''', (recordings[2]['url'], recordings[2]['protectionLevel'], recordings[2]['password'], recordings[2]['meetingID']))
-        self.cursor.execute(''' INSERT INTO recordings(url, protectionLevel, password, meetingID) VALUES (?,?,?,?) ''', (recordings[3]['url'], recordings[3]['protectionLevel'], recordings[3]['password'], recordings[3]['meetingID']))
+        self.cursor.execute(''' INSERT INTO customers(firstName, lastName, phone, email, password) VALUES (?,?,?,?,?) ''', (customers[0]['firstName'], customers[0]['lastName'], customers[0]['phone'], customers[0]['email'], customers[0]['password']))
+        self.cursor.execute(''' INSERT INTO customers(firstName, lastName, phone, email, password) VALUES (?,?,?,?,?) ''', (customers[1]['firstName'], customers[1]['lastName'], customers[1]['phone'], customers[1]['email'], customers[1]['password']))
+        self.cursor.execute(''' INSERT INTO customers(firstName, lastName, phone, email, password) VALUES (?,?,?,?,?) ''', (customers[2]['firstName'], customers[2]['lastName'], customers[2]['phone'], customers[2]['email'], customers[2]['password']))
+        self.cursor.execute(''' INSERT INTO customers(firstName, lastName, phone, email, password) VALUES (?,?,?,?,?) ''', (customers[3]['firstName'], customers[3]['lastName'], customers[3]['phone'], customers[3]['email'], customers[3]['password']))
 
     def testPrint(self):
         print("test print")
